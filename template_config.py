ca8d60f2f98b9740a3d5b3f27e8a00c6956c99f8
ljprust <37141535+ljprust@users.noreply.github.com>
2018-06-13 21:39:25 -0500

    uploaded config

---
+++ template_config.py
@@ -0,0 +1,57 @@
+maxproc = 3
+readpath = '/Users/ljprust/data/'
+writepath = '/Users/ljprust/data/star/plots/'
+simname = ''
+nframes = 
+frameskip = 
+startingset = 
+dDelta = 0.1548
+period = 200
+# CORETEMP
+do_coretemp = 1
+coretemp_title = ''
+# RADPROF
+do_radprof = 1
+radprof_fixaxes = 0
+radprof_axes = [6.0e8, 3.0e11, 3.0e-2, 30.0]
+# TEMPPROF
+do_tempprof = 1
+tempprof_fixaxes = 0
+tempprof_axes = [1.0e9, 1.0e12, 1.0e4, 4.0e7]
+# DENSANIM
+do_densanim = 1
+densanim_direction = 'x'
+densanim_plotwidth = 3.0e11
+densanim_fixlimits = 0
+densanim_lowlim = 6.0e10
+densanim_highlim = 6.0e11
+# PARTSLICEANIM
+do_partslice = 0
+partslice_direction = 'x'
+partslice_parttype = 'H'
+partslice_plotwidth = 3.0e11
+partslice_fixlimits = 0
+partslice_lowlim = 1.0e19
+partslice_highlim = 1.0e24
+# COMPARISON
+do_comparison = 0
+comparison_name = ''
+nplots = 2
+nrows = 1
+ncolumns = 2
+readpath1 = '/Users/ljprust/data/'
+readpath2 = '/Users/ljprust/data/'
+readpath3 = '/Users/ljprust/data/'
+readpath4 = '/Users/ljprust/data/'
+readpath5 = '/Users/ljprust/data/'
+readpath6 = '/Users/ljprust/data/'
+readpath7 = '/Users/ljprust/data/'
+readpath8 = '/Users/ljprust/data/'
+title1 = ''
+title2 = ''
+title3 = ''
+title4 = ''
+title5 = ''
+title6 = ''
+title7 = ''
+title8 = ''
