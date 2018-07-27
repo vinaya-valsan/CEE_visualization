265930561d081234c2524cf470d9ff2d67f44867
Logan Prust <ljprust@uwm.edu>
2018-07-27 16:03:08 -0500

    removed dependencies

--- timestuff.py
+++ timestuff.py
@@ -1,8 +1,6 @@
 from __main__ import *
 import yt
 
-# import mesa_reader as mr
-
 def getTime( ds, iter, cutoff=5.0 ) :
 
 	time = ds.current_time
@@ -16,68 +14,6 @@ def getTime( ds, iter, cutoff=5.0 ) :
 	time = time.in_units(label)
 
 	return time, label
-
-def getMesa( profileFile ) :
-    data = mr.MesaData(profileFile)
-
-    logT   = data.logT
-    mass   = data.mass
-    logR   = data.logR
-    logRho = data.logRho
-    logP   = data.logP
-
-    mass = mass * Msun
-    T = np.power(10.,logT)
-    P = np.power(10.,logP) / G
-    radius = np.power(10.,logR)
-    rSun = 7e10
-    radius = radius * rSun
-    rho = np.power(10.,logRho)
-
-    return T, mass, radius, rho, P
-
-def getMesaEnt( profileFile ) :
-	data = mr.MesaData(profileFile)
-
-	logT = data.logT
-	mass = data.mass
-	mass = mass*Msun
-	logP = data.logP
-	logR = data.logR
-
-	T = np.power(10.,logT)
-	P = np.power(10.,logP) / G
-
-	dm = np.zeros(len(mass))
-	for i in range(0,len(mass)-1) :
-		dm[i] = mass[i] - mass[i+1]
-	dm[len(mass)-1] = mass[len(mass)-1]
-
-	ent = R * ( 5./2. + np.log( np.power(k*T,2.5) / P /h/h/h * np.power(2. * math.pi * mpart, 1.5) ) )
-
-	return ent, dm
-
-def getMesaEnt2( profileFile ) :
-	data = mr.MesaData(profileFile)
-
-	logT = data.logT
-	mass = data.mass
-	mass = mass*Msun
-	logP = data.logP
-	logR = data.logR
-	logRho = data.logRho
-
-	P = np.power(10.,logP) / G
-	rho = np.power(10.,logRho)
-
-	# dm = np.zeros(len(mass))
-	# for i in range(0,len(mass)-1) :
-	# 	dm[i] = mass[i] - mass[i+1]
-	# dm[len(mass)-1] = mass[len(mass)-1]
-
-	ent = np.divide( P, np.power( rho, gamma ) )
-
-	return ent
 
 def getEnt( ds ) :
 	ad = ds.all_data()
